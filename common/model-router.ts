d6b05484c607829ef6e8eb1c44bdfec2ca8b71a5
Luis Henrique <luishenrique.mf01@gmail.com>
2021-01-08 00:31:48 -0300

    paginaçao skip e limit

--- common/model-router.ts
+++ common/model-router.ts
@@ -3,16 +3,46 @@ import * as mongoose from 'mongoose';
 import { NotFoundError } from 'restify-errors';
 
 export abstract class ModelRouter<D extends mongoose.Document> extends Router {
+  basePath: string;
+
+  pageSize: number = 2;
+
   constructor(protected model: mongoose.Model<D>) {
     super();
+    this.basePath = `/${this.model.collection.name}`;
   }
 
-  protected prepareOne(query: mongoose.DocumentQuery<D,D>): mongoose.DocumentQuery<D,D> {
+  protected prepareOne(query: mongoose.DocumentQuery<D, D>): mongoose.DocumentQuery<D, D> {
     return query;
   };
 
+  envelope(document: any): any {
+    let resource = Object.assign({ _links: {} }, document.toJSON());
+    resource._links.self = `${this.basePath}/${resource._id}`;
+    return resource;
+  };
+
+  envelopeAll(documents: any[], options: any = {}): any {
+    const resource: any = {
+      _links: {
+        self: `${options.url}`
+      },
+      items: documents,
+    };
+    if (options.page && options.count && options.pageSize) {
+      if (options.page) {
+        resource._links.previous = `${this.basePath}?_page=${options.page - 1}`;
+      };
+      const remaining = options.count - (options.page * options.pageSize);
+      if (remaining > 0) {
+        resource._links.next = `${this.basePath}?_page=${options.page + 1}`;
+      };
+    };
+    return resource;
+  };
+
   validateId = (req, resp, next) => {
-    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
       next(new NotFoundError('Document not found'));
     } else {
       next();
@@ -20,23 +50,34 @@ export abstract class ModelRouter<D extends mongoose.Document> extends Router {
   };
 
   findAll = (req, resp, next) => {
-    this.model.find()
-      .then(this.renderAll(resp,next))
+    let page = parseInt(req.query._page || 1);
+    page = page > 0 ? page : 1;
+
+    const skip = (page - 1) * this.pageSize;
+
+    this.model
+      .count({}).exec()
+      .then(count => this.model.find()
+        .skip(skip)
+        .limit(this.pageSize)
+        .then(this.renderAll(resp, next, {
+          page, count, pageSize: this.pageSize, url: req.url
+        })))
       .catch(next);
   };
 
   findById = (req, resp, next) => {
     this.prepareOne(this.model.findById(req.params.id))
-      .then(this.render(resp,next))
+      .then(this.render(resp, next))
       .catch(next);
   };
 
   save = (req, resp, next) => {
     let document = new this.model(req.body);
     document.save()
-      .then(this.render(resp,next))
+      .then(this.render(resp, next))
       .catch(next);
-  
+
   };
 
   replace = (req, resp, next) => {
@@ -48,14 +89,14 @@ export abstract class ModelRouter<D extends mongoose.Document> extends Router {
         } else {
           throw new NotFoundError('Document not found.');
         }
-      }).then(this.render(resp,next))
-        .catch(next);
+      }).then(this.render(resp, next))
+      .catch(next);
   };
 
   update = (req, resp, next) => {
     const options = { runValidators: true, new: true };
     this.model.findByIdAndUpdate(req.params.id, req.body, options)
-      .then(this.render(resp,next))
+      .then(this.render(resp, next))
       .catch(next);
   };
 
--- users/users.router.ts
+++ users/users.router.ts
@@ -30,15 +30,15 @@ class UserRouter extends ModelRouter<User> {
   };
 
   applyRouters(application: restify.Server) {
-    application.get('/users', restify.plugins.conditionalHandler([
-      { version: '2.0.0', handler: [this.findByEmail, this.findAll] },
-      { version: '1.0.0', handler: this.findAll },
+    application.get(`${this.basePath}`, restify.plugins.conditionalHandler([
+      { version: `2.0.0`, handler: [this.findByEmail, this.findAll] },
+      { version: `1.0.0`, handler: this.findAll },
     ]));
-    application.get('/users/:id', [ this.validateId, this.findById ]);
-    application.post('/users', [ this.validateId, this.save ]);
-    application.put('/users/:id', [ this.validateId, this.replace ]);
-    application.patch('/users/:id', [ this.validateId, this.update ]);
-    application.del('/users/:id', [ this.validateId, this.delete ]);
+    application.get(`${this.basePath}/:id`, [ this.validateId, this.findById ]);
+    application.post(`${this.basePath}`, [ this.validateId, this.save ]);
+    application.put(`${this.basePath}/:id`, [ this.validateId, this.replace ]);
+    application.patch(`${this.basePath}/:id`, [ this.validateId, this.update ]);
+    application.del(`${this.basePath}/:id`, [ this.validateId, this.delete ]);
   }
 }
 
